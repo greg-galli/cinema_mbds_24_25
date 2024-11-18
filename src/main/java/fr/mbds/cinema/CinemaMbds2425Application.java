013dd081406ec0c616cd0d18e92db9428fbb308c
Gregory Galli <greg.galli@gmail.com>
2024-11-18 16:02:58 +0100

    first commit

---
+++ src/main/java/fr/mbds/cinema/CinemaMbds2425Application.java
@@ -0,0 +1,13 @@
+package fr.mbds.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaMbds2425Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaMbds2425Application.class, args);
+    }
+
+}
---
+++ src/test/java/fr/mbds/cinema/CinemaMbds2425ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mbds.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaMbds2425ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
